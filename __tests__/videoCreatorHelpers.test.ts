13a8f600f6651206a74837899f1ca4fc180fb191
Your Name <you@example.com>
2025-03-13 12:42:55 +0100

    Add

--- __tests__/videoCreatorHelpers.test.ts
+++ __tests__/videoCreatorHelpers.test.ts
@@ -9,21 +9,42 @@ describe('VideoCreator Helpers - Integration Tests (with real Kafka broker)', ()
   describe('sendVideoCreationMessage', () => {
     it('should send a message to the Kafka topic and be consumed', async () => {
       const payload = {
-        correlationId: `test-correlation-id-${Date.now()}`,
-        videoSize: [1280, 720],
-        duration: 60,
-        textConfig: { font: 'Verdana' },
+        correlationId: '0e872abb-f212-4b25-91cb-9a576e681cdd',
+        speechFile: 'ac7857-speech.aac',
+        musicFile: '7be7b7-emo.mp3',
+        imageFiles: [
+          'd28727-puppy_0.jpg',
+          '566118-puppy_1.jpg',
+          'f43300-puppy_2.jpg',
+          'd52970-puppy_4.jpg',
+          '5b7e0f-puppy_5.jpg'
+        ],
+        videoSize: [1920, 1080],
+        duration: 15,
+        textConfig: { font_color: 'white', background_color: 'black' },
         fps: 24,
-        textData: [{ text: 'Test' }]
+        textData: [
+          { word: 'Ladybird', start: 0, end: 0.44 },
+          { word: 'là', start: 0.44, end: 0.66 },
+          { word: 'một', start: 0.66, end: 0.82 },
+          { word: 'trình', start: 0.82, end: 1 },
+          { word: 'duyệt', start: 1, end: 1.26 },
+          { word: 'web', start: 1.38, end: 1.48 },
+          { word: 'độc', start: 1.48, end: 1.9 },
+          { word: 'lập', start: 1.9, end: 2.08 },
+          { word: 'và', start: 2.26, end: 2.42 },
+          { word: 'mã', start: 2.42, end: 2.68 },
+          { word: 'nguồn', start: 2.68, end: 2.94 },
+          { word: 'mở', start: 2.94, end: 3.1 },
+          { word: 'được', start: 3.78, end: 3.86 },
+          { word: 'phát', start: 3.86, end: 4.08 },
+          { word: 'triển', start: 4.12, end: 4.28 },
+          { word: 'bởi', start: 4.28, end: 4.46 },
+          { word: 'Ladybird', start: 4.46, end: 4.9 },
+          { word: 'Browser', start: 4.9, end: 5.34 },
+          { word: 'Initiative', start: 5.78, end: 5.88 }
+        ]
       };
-
-      const topic = 'video-creation-requests'; // Replace with your actual topic name
-
-      const messages: Array<{
-        key: string | undefined;
-        value: string | undefined;
-        headers: Record<string, any> | undefined;
-      }> = [];
 
       await sendVideoCreationMessage(payload);
       const queueName = config.rabbitmq.taskQueue;
